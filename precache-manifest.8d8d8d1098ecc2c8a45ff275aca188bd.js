ce6fc581892a4c068d050b401591d3fb4e1290cd
Ramon Fávero <ramon.favero@hotmail.com>
2023-03-14 18:46:15 -0300

    Updates

---
+++ precache-manifest.8d8d8d1098ecc2c8a45ff275aca188bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968972282353d7ebee4eaf0c3b42f5ee",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "97477a5c20e2980af247",
+    "url": "/KeeperApp/static/js/2.19f56d25.chunk.js"
+  },
+  {
+    "revision": "6f9224876610881134f9",
+    "url": "/KeeperApp/static/js/main.2106cc16.chunk.js"
+  },
+  {
+    "revision": "95227ae8f6c791d81ed1",
+    "url": "/KeeperApp/static/js/runtime-main.c1dadce6.js"
+  }
+]);
